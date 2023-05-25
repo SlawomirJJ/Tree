a3f1f5285f843858bfc340e8f83969f7aff82fd0
SlawomirJJ <81370053+SlawomirJJ@users.noreply.github.com>
2023-05-26 00:08:01 +0200

    Dodaj pliki projektów.

---
+++ Tree/Entities/File.cs
@@ -0,0 +1,11 @@
+﻿namespace Tree.Entities
+{
+    public class File
+    {
+        public int Id { get; set; }
+        public string Path { get; set; }
+        public string Name { get; set; }
+        public string Format { get; set; }
+
+    }
+}
---
+++ Tree/Entities/TreeDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Tree.Entities
+{
+    public class TreeDbContext: DbContext
+    {
+        public DbSet <File> Files { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<File>()
+                .Property(r => r.Name)
+                .IsRequired();
+
+            modelBuilder.Entity<File>()
+                .Property(r => r.Path)
+                .IsRequired();
+
+            modelBuilder.Entity<File>()
+                .Property(r => r.Format)
+                .IsRequired();
+        }
+    }
+}
